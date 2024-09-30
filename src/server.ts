6500a32f2e876247aaa22477379b8a5e25ec5a3b
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-09-30 16:00:54 +0200

    Added GET users

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import http from "http";
+import { sql } from "./database";
 
 export function createServer() {
   const app = express();
 
   app.get("/_health", (req, res, next) => {
     res.send("OK");
+  });
+
+  app.get("/users", async (req, res, next) => {
+    const users = await sql.query.users.findMany({
+      with: {
+        posts: true,
+      },
+    });
+
+    res.send(users);
   });
 
   app.use((req, res, next) => {
