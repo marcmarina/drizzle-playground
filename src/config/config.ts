aacab5fa79ced494a4ecef10e7f6284500286f4d
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-10-08 11:02:20 +0200

    Added logger

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,10 @@
-import { integer } from "./helpers";
+import { integer, oneOf } from "./helpers";
 
 export const server = {
   port: integer("PORT"),
+};
+
+export const logger = {
+  level: oneOf("LOG_LEVEL", ["silent", "debug", "info", "warn", "error"]),
+  format: oneOf("LOG_FORMAT", ["pretty", "json"]),
 };
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,20 @@ import "dotenv/config";
 
 import { config } from "./config";
 import { createServer } from "./server";
+import { logger } from "./logger";
 
 async function main() {
   const server = createServer();
 
   server.listen(config.server.port, () => {
-    console.log(`Server listening on port ${config.server.port}`);
+    logger.info(`Server listening on port ${config.server.port}`);
   });
 
   const exitSignals: NodeJS.Signals[] = ["SIGINT", "SIGTERM"];
 
   exitSignals.forEach((s) => {
     process.on(s, () => {
-      console.log(`${s} signal received. Closing server.`);
+      logger.info(`${s} signal received. Closing server.`);
 
       server.close(() => {
         process.exit(1);
