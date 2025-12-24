54a1cb33b8a8a56ae0157d7f031296d2d725d77d
Marc Marina <mmarina@findmypast.com>
2025-12-24 12:51:06 +0100

    Deleted migration script

--- src/database/migrate.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-
-import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { sql } from "./database";
-
-migrate(sql, {
-  migrationsFolder: "./src/database/drizzle",
-});
